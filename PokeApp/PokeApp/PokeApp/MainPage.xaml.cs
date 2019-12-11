1e0168378f71bf79b405fb83bdf96ccad57f75d1
unknown <juergen.riegler@gmx.net>
2019-12-11 17:17:00 +0100

    update

---
+++ PokeApp/PokeApp/PokeApp/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using PokeApp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+using Newtonsoft.Json;
+
+namespace PokeApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+
+        public Pokemon Pokemon { get; set; }
+        private static HttpClient HttpClient
+           = new HttpClient() { BaseAddress = new Uri("https://pokeapi.co/api/v2/") };
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+            GetPokemon();
+        }
+
+        public async void GetPokemon()
+        {
+            var pokemonResponse = await HttpClient.GetAsync("pokemon");
+            pokemonResponse.EnsureSuccessStatusCode();
+            var responseBody = await pokemonResponse.Content.ReadAsStringAsync();
+            var pokemon = JsonConvert.DeserializeObject<Pokemon>(responseBody);
+        }
+    }
+}
---
+++ PokeApp/PokeApp/PokeApp/Models/Pokemon.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokeApp.Models
+{
+    public partial class Pokemon
+    {
+        [JsonProperty("count")]
+        public long Count { get; set; }
+
+        [JsonProperty("next")]
+        public Uri Next { get; set; }
+
+        [JsonProperty("previous")]
+        public object Previous { get; set; }
+
+        [JsonProperty("results")]
+        public Result[] Results { get; set; }
+    }
+
+    public partial class Result
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("url")]
+        public Uri Url { get; set; }
+    }
+}
